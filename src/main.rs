32e849d19edec803c3808b5b9826dfc9c2888440
Aaron <aaronkanaron@gmail.com>
2025-07-19 17:00:52 +0200

    Test commit

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "Planet Game".to_string(),
+                title: "Planet Game 2".to_string(),
                 resolution: (800.0, 600.0).into(),
                 ..default()
             }),
