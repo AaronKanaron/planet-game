59f7fd81657f9bfe27cca30f1eb5ffd89daf645e
Arturr-H <artur.h.0427@gmail.com>
2025-08-06 18:49:43 +0200

    feat: Add `DebugRotate` component and system for entity rotation (debug vis)

--- src/utils/debug/plugin.rs
+++ src/utils/debug/plugin.rs
@@ -1,9 +1,21 @@
 use crate::utils::debug::framerate_overlay::FpsOverlayPlugin;
 use bevy::prelude::*;
 
+/// Just a simple debug rotate thing for checking entities
+#[derive(Component)]
+pub struct DebugRotate;
+impl DebugRotate {
+    pub fn update(mut query: Query<(&mut Transform, &DebugRotate)>, time: Res<Time>) {
+        for (mut transform, _) in query.iter_mut() {
+            transform.rotate(Quat::from_rotation_y(time.delta_secs()));
+        }
+    }
+}
+
 pub struct DebugPlugin;
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(FpsOverlayPlugin);
+        app.add_plugins(FpsOverlayPlugin)
+            .add_systems(Update, DebugRotate::update);
     }
 }
